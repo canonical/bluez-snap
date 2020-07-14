93677f5ccc0f0441e950457f5bb6f9f6c77e8087
Alfonso Sánchez-Beato <alfonso.sanchez-beato@canonical.com>
2020-07-14 14:02:35 +0200

    tests: run tests now on UC20 image

--- tests/lib/prepare.sh
+++ tests/lib/prepare.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
+#!/bin/bash -ex
 
-. $TESTSLIB/utilities.sh
+# shellcheck source=tests/lib/utilities.sh
+. "$TESTSLIB"/utilities.sh
 
 echo "Wait for firstboot change to be ready"
 while ! snap changes | grep -q "Done"; do
@@ -10,9 +11,10 @@ while ! snap changes | grep -q "Done"; do
 done
 
 echo "Ensure fundamental snaps are still present"
-. $TESTSLIB/snap-names.sh
-for name in $gadget_name $kernel_name $core_name; do
-	if ! snap list | grep -q $name ; then
+# shellcheck source=tests/lib/snap-names.sh
+. "$TESTSLIB"/snap-names.sh
+for name in "$gadget_name" "$kernel_name" "$core_name"; do
+	if ! snap list | grep -q "$name" ; then
 		echo "Not all fundamental snaps are available, all-snap image not valid"
 		echo "Currently installed snaps:"
 		snap list
@@ -22,13 +24,6 @@ done
 
 install_snap_under_test
 
-# Snapshot of the current snapd state for a later restore
-if [ ! -f $SPREAD_PATH/snapd-state.tar.gz ] ; then
-	systemctl stop snapd.service snapd.socket
-	tar czf $SPREAD_PATH/snapd-state.tar.gz /var/lib/snapd /etc/netplan
-	systemctl start snapd.socket
-fi
-
 # For debugging dump all snaps and connected slots/plugs
 snap list
-snap interfaces
+snap connections --all
